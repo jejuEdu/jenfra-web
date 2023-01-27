937d3abb72d1543b21eb47ba276694e726b13113
ohcho-dev <ohcho.dev@gmail.com>
2023-01-27 23:48:08 +0900

    chore: 필요없는 페이지 삭제, 로딩스피너 컴포넌트 생성

---
+++ components/common/LoadingSpinner/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LoadingSpinner = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  top: 0;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
