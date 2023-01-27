ee7817bb28d18f96287a99326356d41c8a04c43f
ohcho-dev <ohcho.dev@gmail.com>
2023-01-27 23:49:18 +0900

    feat: react-query 설정

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,21 @@ import '../styles/reset.css';
 import '../styles/globals.css';
 import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function MyApp({ Component, pageProps }) {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <>
+    <QueryClientProvider client={client}>
+      {/* {process.env.NODE_ENV !== 'production' ? <ReactQueryDevtools initialIsOpen={false} /> : null} */}
       <Head>
         {/* 더블클릭이나 pinch zoom을 통한 확대/축소 기능 막기 */}
         <meta
@@ -17,7 +28,7 @@ function MyApp({ Component, pageProps }) {
       <RecoilRoot>
         <Component {...pageProps} />
       </RecoilRoot>
-    </>
+    </QueryClientProvider>
   );
 }
 
