174b90b1715e7e2a4eac7faa5cd5e0e42752c1fa
ohcho-dev <ohcho.dev@gmail.com>
2023-02-05 11:02:30 +0900

    fix: index page import 경로 변경

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import LayoutMain from '../layouts/LayoutMain';
 import Main from '../components/main';
 import { COLORS } from '../constant/colors';
-import * as S from './style';
+import * as S from '../styles/style';
 
 export default function Home() {
   return (
