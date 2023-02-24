d35c562482be0d45f9ced3d1ebecfff5cebc4da5
ohcho-dev <ohcho.dev@gmail.com>
2023-02-24 20:05:47 +0900

    fix: mainpage style 수정

--- styles/style.js
+++ styles/style.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const Header = styled.div`
-  padding: 2.8rem 0 6.1rem;
+  padding: 2.5rem 0 3rem;
   text-align: center;
 `;
 
 export const Footer = styled.div`
   background: white;
   width: 100%;
-  padding: 5.2rem;
-  border-radius: 3rem 3rem 0 0;
+  padding: 2rem;
+  border-radius: 2rem 2rem 0 0;
   text-align: center;
   position: absolute;
   bottom: 0;
